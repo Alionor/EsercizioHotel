0a709e2cfb5b4d8336c00d76c4a8b61ecc96f5a0
Alionor <elegrisa32@libero.it>
2026-03-02 15:48:33 +0100

    Classe Hotel impostata

--- src/it/prova/hotel/model/Hotel.java
+++ src/it/prova/hotel/model/Hotel.java
@@ -1,4 +1,39 @@
 package it.prova.hotel.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hotel {
+
+    private String ragioneSociale;
+    private int stelle;
+    private List<Stanza> stanze = new ArrayList<>();
+
+    public Hotel(String ragioneSociale, int stelle) {
+        this.ragioneSociale = ragioneSociale;
+        this.stelle = stelle;
+    }
+
+    public Hotel() {
+    }
+
+    public String getRagioneSociale() {
+        return ragioneSociale;
+    }
+
+    public void setRagioneSociale(String ragioneSociale) {
+        this.ragioneSociale = ragioneSociale;
+    }
+
+    public int getStelle() {
+        return stelle;
+    }
+
+    public void setStelle(int stelle) {
+        this.stelle = stelle;
+    }
+
+   /* public float calcolaConto(Stanza input) {
+    }*/
+
 }
